1945e2d9968d79fbad011f8eb7cd5ca904191b82
nicobonder <nicobonder@hotmail.com>
2022-02-15 12:38:13 -0300

    change in contact

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -2,22 +2,40 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 
 export default function App() {
-    const { register, formState: { errors }, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, 
+            handleSubmit,
+            formState: { errors }  
+        } = useForm({
+            defaultValues: {
+                name: "",
+                email: ""
+            }
+        });
+    /*const onSubmit = data => console.log(data);*/
    
   return (
     <form onSubmit={handleSubmit((data) => {
         console.log(data);
     })}
     >
-      <input {...register("name", { required: true })} placeholder="Nombre" />
-      {errors.name?.type === 'required' && "Name is required"}
+      <input {...register("name", { 
+          required: 'El nombre es necesario',
+          minLength: {value: 4,
+          message: "Por lo menos 4 letras"}
+      })} placeholder="Nombre" />
+      <p>{errors.name?.message}</p>
 
-      <input {...register("email", { required: true })} placeholder="Email" />
-      {errors.email?.type === 'required' && "Email is required"}
+      <input {...register("email", { required: 'Por favor un mail' })} 
+      placeholder="Email" />
+      <p>{errors.email?.message}</p>
 
-      <input {...register("text", { required: true })} placeholder="Dejame tu mensaje"/>
-      {errors.text?.type === 'required' && "Please, enter your message"}
+      <input {...register("text", { required: 'Por favor, ingresa un mensaje', 
+      minLength: {
+          value: 20,
+          message: "Por lo menos 20 letras"} 
+        })} 
+      placeholder="Dejame tu mensaje"/>
+      <p>{errors.text?.message}</p>
 
       <input type="submit" />
     </form>

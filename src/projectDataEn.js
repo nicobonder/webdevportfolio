1487d933f91b0d5364f98a4efdbbb356b671bde8
nicobonder <nicobonder@hotmail.com>
2022-05-11 08:48:31 -0300

    gout included

--- src/projectDataEn.js
+++ src/projectDataEn.js
@@ -17,15 +17,15 @@ const projectsEn = [
     description: "I work as a content writer, so I made a web where I can show my services and my expertise. I created it using HTML, CSS and JavaScript. I also used the Animate On Scroll Library and the Typed.js Library.",
     img: "",
     },
-    /*{id: 3,
+    {id: 3,
     clase: "gout",
     link: "https://gout.vercel.app/",
     title: "Click to open this project",
     name: "goût",
     description: "Group project in which we created a social network dedicated to gastronomy. We used React.js, Material UI and Redux Toolkit in the Front End and Node Js, MySQL and Cloudinary in the Back End.",
     img: "",
-    },*/
-    {id: 3,
+    },
+    /*{id: 3,
     clase: "gout",
     link: "http://altoviaje.herokuapp.com/",
     title: "Click to open this project",
@@ -40,7 +40,7 @@ const projectsEn = [
     name: "Coming soon!",
     description: "ecommerce to sell travelers ebooks. It was created with React.js and Material UI. It is Under Construction, but will be ready soon!",
     img: "",
-    },
+    },*/
 ]
 
 export default projectsEn;
